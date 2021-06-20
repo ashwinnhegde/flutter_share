c009befae48494e04b6984f9fe4ced2aabd45d00
Ashwin Hegde <ashwinnhegde@gmail.com>
2021-06-20 23:54:28 +0530

    changes

--- lib/flutter_social_content_share.dart
+++ lib/flutter_social_content_share.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-enum ShareType { facebookWithoutImage, instagramWithImageUrl, more }
+enum ShareType { facebookWithoutImage, instagramWithImageUrl, share }
 
 class FlutterSocialContentShare {
   static const MethodChannel _channel =
